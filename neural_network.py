c7972add82f53d0e73dbe7155707afb52540a349
mocurin <elleyzzzzz@gmail.com>
2019-08-15 14:02:14 +0300

    Complete rework

--- neural_network.py
+++ neural_network.py
@@ -2,38 +2,44 @@ import functools
 import numpy
 
 
-class neural_network:
-    def __init__(self, inp, outp):
-        self._shape = (inp, outp)
-        self._param = ([], [], [])
+def linear_handler(parameters, input_data):
+    return numpy.array(parameters['matrix']).dot(input_data) + parameters['bias']
 
-    def add_layer(self, string, *, matrix=None, bias=None, func=None):
-        if string == 'linear':
-            assert matrix and bias and func is None
-            self._param[0].append(numpy.array(matrix))
-            self._param[1].append(numpy.array(bias))
-        elif string == 'function':
-            assert func and matrix is None and bias is None
-            self._param[2].append(func)
 
-    def check_dimensions(self):
-        current = self._shape[0]
-        for i in zip(self._param[0], self._param[1]):
-            if i[0].shape[1] != current or i[0].shape[0] != i[1].shape[0]:
-                return False
-            current = i[0].shape[0]
-        return current == self._shape[1]
+def function_handler(parameters, input_data):
+    return list(map(parameters['func'], input_data))
 
-    def compute(self, input):
-        return functools.reduce(
-            lambda a, x: list(
-                map(
-                    x[2],
-                    (x[0].dot(a) + x[1])
-                )
-            ), [input] + [list(i) for i in zip(*self._param)]
-        )
+
+handlers = {'linear': linear_handler, 'function': function_handler}
+
+
+class layer:
+    def __init__(self, layer_type, parameters):
+        self._parameters = parameters
+        self._handler = handlers[layer_type]
+
+    def __call__(self, input_data):
+        return self._handler(self._parameters, input_data)
 
 
 def make_feedforward_neural_network(*, input_count, output_count):
     return neural_network(input_count, output_count)
+
+
+class neural_network:
+    def __init__(self, input_count, output_count):
+        self._shape = self._input, self._output = input_count, output_count
+        self._layers = []
+
+    def add_layer(self, layer_type, **parameters):
+        self._layers.append(layer(layer_type, parameters))
+
+    def check_dimensions(self):
+        try:
+            result = self([0] * self._input)
+        except:
+            return False
+        return len(result) == self._output
+
+    def __call__(self, input_data):
+        return functools.reduce(lambda prev, current: current(prev), [input_data] + self._layers)
--- tests.py
+++ tests.py
@@ -13,10 +13,10 @@ class NeuralNetworkTest(unittest.TestCase):
         nn.add_layer('linear', matrix=[[1, 1]], bias=[-1])
         nn.add_layer('function', func=rectifier)
         self.assertTrue(nn.check_dimensions())
-        self.assertEqual(nn.compute([0, 0]), [0])
-        self.assertEqual(nn.compute([0, 1]), [0])
-        self.assertEqual(nn.compute([1, 0]), [0])
-        self.assertEqual(nn.compute([1, 1]), [1])
+        self.assertEqual(nn([0, 0]), [0])
+        self.assertEqual(nn([0, 1]), [0])
+        self.assertEqual(nn([1, 0]), [0])
+        self.assertEqual(nn([1, 1]), [1])
 
     def test_or(self):
         zero_threshold = make_threshold(0)
@@ -24,25 +24,25 @@ class NeuralNetworkTest(unittest.TestCase):
         nn.add_layer('linear', matrix=[[1, 1]], bias=[0])
         nn.add_layer('function', func=zero_threshold)
         self.assertTrue(nn.check_dimensions())
-        self.assertEqual(nn.compute([0, 0]), [0])
-        self.assertEqual(nn.compute([0, 1]), [1])
-        self.assertEqual(nn.compute([1, 0]), [1])
-        self.assertEqual(nn.compute([1, 1]), [1])
+        self.assertEqual(nn([0, 0]), [0])
+        self.assertEqual(nn([0, 1]), [1])
+        self.assertEqual(nn([1, 0]), [1])
+        self.assertEqual(nn([1, 1]), [1])
 
     def test_xor(self):
         threshold = make_threshold(0.5)
         nn = neural.make_feedforward_neural_network(input_count=2, output_count=1)
-        nn.add_layer('linear', matrix=[[1, -1], [-1, 1]], bias=[0, 0])
+        nn.add_layer('linear', matrix=[[1, -1],
+                                       [-1, 1]], bias=[0, 0])
         nn.add_layer('function', func=threshold)
         nn.add_layer('linear', matrix=[[1, 1]], bias=[0])
         nn.add_layer('function', func=threshold)
         self.assertTrue(nn.check_dimensions())
-        self.assertEqual(nn.compute([0, 0]), [0])
-        self.assertEqual(nn.compute([0, 1]), [1])
-        self.assertEqual(nn.compute([1, 0]), [1])
-        self.assertEqual(nn.compute([1, 1]), [0])
-
-
+        self.assertEqual(nn([0, 0]), [0])
+        self.assertEqual(nn([0, 1]), [1])
+        self.assertEqual(nn([1, 0]), [1])
+        self.assertEqual(nn([1, 1]), [0])
 
 if __name__ == '__main__':
     unittest.main()
+
